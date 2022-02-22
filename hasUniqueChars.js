f84e778d36a13f55a9f9de014f44340e9993d004
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-02-21 23:17:12 -0700

    the code works and has the runtime

---
+++ hasUniqueChars.js
@@ -0,0 +1,15 @@
+function uniqueLetters(word) {
+    let checkArr = new Set([])
+    for (i = 0; i < word.length; i++) {
+        checkArr.add(word[i])
+    }
+    return (word.length === checkArr.size)
+}
+
+console.log(uniqueLetters("asdq"))
+console.log(uniqueLetters("add"))
+
+console.log(uniqueLetters("apple"))
+console.log(uniqueLetters("sponge"))
+
+//This is O(n)
