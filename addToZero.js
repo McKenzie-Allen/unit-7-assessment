cb8d9619e57138bf70b9f7172f39683a62beb349
McKenzie Allen <mckenzie.allen144@gmail.com>
2022-02-21 23:06:58 -0700

    first commit

---
+++ addToZero.js
@@ -0,0 +1,17 @@
+let array = [28, 43, -11, 30, 4, 0, 12]
+
+function addToZero(arr) {
+    let addZero = "false"
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let x = i; x >= 0; x--) {
+            if (i !== x) {
+                if (arr[i] + arr[x] === 0) {
+                    addZero = "true"
+                }
+            }
+        }
+    }
+    return addZero
+}
+
+console.log(addToZero(array))
